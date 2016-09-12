c7aee5621faa9619251481649aa16ca956263b56
GD <externalbuffer@gmail.com>
2016-09-12 16:14:06 +0300

    funct Calc

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -2,44 +2,44 @@
 
 #ar = input("enter an arithmetic expression: ")
 
-expr = '7+5.2*4+2'
+expr = '7+5*4'
 print (expr)
 
-listExp = []
 
-
+# breaking Tokens
 def BreakTokens(expr):
     startSlice = 0
     count = 0    
-
-    def AddToken(start, end, char):
-        listExp.append(expr[start : end])
-        listExp.append(char)
-
-   # def AddToken(start, end):
-   #     listExp.append(expr[start : end])
-        
-
+    listExp = []
     for i in expr:
         if i == '+' or i == '*':
-            AddToken(startSlice, count, i)                        
+            listExp.append(expr[startSlice:count])
+            listExp.append(i)                                  
             startSlice = count + 1                
         count = count + 1
-    AddToken(startSlice, len(expr)-1)                        
+    listExp.append(expr[startSlice: len(expr)])                              
+    return listExp
 
-    print(listExp)  
-    return True
-
-BreakTokens(expr)
+listExp = BreakTokens(expr)
+print(listExp)
 
 def Calc(startInd, endInd):
-
-    return True
+    for i in range(startInd, endInd+1):
+        if listExp[i] == '+':
+            rez = Calc(startInd, i-1) + Calc(i+1, endInd);
+        elif listExp[i] == '*':
+            rez = Calc(startInd, i-1) * Calc(i+1, endInd);
+        
+        if (endInd == startInd):
+            rez = int(listExp[i])
+            
+    #print('calc')
+    return rez
 
 stExpr = 0;
 endExpr = len(listExp)
 
-rez = Calc(startExpr, endExpr) 
+rez = Calc(stExpr, endExpr) 
 
 
 
