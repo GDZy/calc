31cc155784f9ca605c3075b496196b7b429bb842
GD <externalbuffer@gmail.com>
2016-09-15 16:18:05 +0300

    stack

--- PythonApplication1/stack.py
+++ PythonApplication1/stack.py
@@ -19,7 +19,7 @@ class Stack(object):
         return len(self.items)          
     
     def isEmpty (self):
-        return self.items               
+        return self.items == []              
     
     def last (self):
         return self.items[-1]
